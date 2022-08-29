4edf5897a519b908e30686d4ea2728ff3f374ffd
ranna <jran0323@gmail.com>
2022-08-29 17:55:44 +0900

    Update 21_number_to_name.js

--- 04_making_decision/21_number_to_name.js
+++ 04_making_decision/21_number_to_name.js
@@ -1,29 +1,31 @@
+// 다국어 수정
 const rd = require('../util/util-question');
 
 (async () => {
     let result;
     let inputNumber = await rd.getNumber('Please enter the number of the month: ')
 
-    if(inputNumber && numberToNameDic[inputNumber]){
-        result = numberToNameDic[inputNumber]
+    if(inputNumber && numberToNameMap.keys()){
+        result = numberToNameMap.get(inputNumber)
         console.log(`The name of the month is ${result}`)
-    } else {
+    }else {
         console.log('wrong number')
     }
     rd.close();
 })()
 
-const numberToNameDic = {
-    '1' : 'Jan',
-    '2' : 'Feb',
-    '3' : 'Mar',
-    '4' : 'Apr',
-    '5' : 'May',
-    '6' : 'Jun',
-    '7' : 'Jul',
-    '8' : 'Aug',
-    '9' : 'Sep',
-    '10' : 'Oct',
-    '11' : 'Nov',
-    '12' : 'Dec'
-}
+//Switch-case문 제거
+const numberToNameMap = new Map([
+    [1, 'January'],
+    [2, 'February'],
+    [3, 'March'],
+    [4, 'April'],
+    [5, 'May'],
+    [6, 'June'],
+    [7, 'July'],
+    [8, 'August'],
+    [9, 'September'],
+    [10, 'October'],
+    [11, 'November'],
+    [12, 'December'],
+])
