9db90a8eecb151cff042bb7a4930eb084640c2b1
ranna <jran0323@gmail.com>
2022-09-06 18:03:59 +0900

    Update 32_guess_the_number_game.js

--- 06_repetition/32_guess_the_number_game.js
+++ 06_repetition/32_guess_the_number_game.js
@@ -1,52 +1,69 @@
+// 사용자가 입력한 숫자를 모두 기록하기 - 수정필요
 const rd = require('../util/util-question');
 
 (async () => {
-    console.log(`Let's play Guess the number. ` )
-    const level = parseInt(await rd.getNumber(`Pick a difficulty level (1,2, or 3): `))
+    while (true) {
+        console.log(`Let's play Guess the number. ` )
 
-    if (level < 1 || level > 3) {
-        console.log(`"${level}" is wrong level`);
-    }
-
-
-    if(level === 1) {
-        let randomNumber = parseInt(Math.random() * 10 + 1);// 1~10 random Number
-        let tryingcount = 0// 시도한 횟수
-        let wrongCount = 0; //잘못 시도한 횟수
-        let guessInput = await rd.question(`I have my number. What's your guess? `);
-        tryingcount++;
-
-        while(true) {
-            if(guessInput == " " || /[a-zA-Z]/.test(guessInput)) {
-                console.log(`숫자를 입력하세요`)
-                wrongCount++;
-
-            }
-            else if(guessInput == randomNumber){
-                console.log(`Success`)
+        let level;
+        while (true) {
+            level = parseInt(await rd.getNumber(`Pick a difficulty level (1,2, or 3): `))
+            if (level < 1 || level > 3) {
+                console.log(`"${level}" is wrong level`);
+            } else{
                 break;
             }
-            else {
-                if(randomNumber > guessInput || randomNumber < guessInput){
-                    guessInput = await rd.question(
-                        `${randomNumber > guessInput ? 'Too row' : 'Too high '}. Guess again: 
-                    `)
-                    tryingcount++;
-                    continue;
+        }
+        let randomNumber = parseInt(Math.random()*Math.pow(10, level));// 1~10의 level승까지의 random Number
+        let tryingCount = 0// 시도한 횟수
+        let wrongCount = 0; //잘못 시도한 횟수
+
+        let guessInput;
+        while (true) {
+            guessInput = await rd.question(`I have my number. What's your guess? `);
+            tryingCount++;
+            while(true){
+                if(guessInput == " " || /[a-zA-Z]/.test(guessInput)) {
+                    wrongCount++;
+                    console.log(`"${guessInput}" is not a number, Wrong count: ${wrongCount}`)
+                    break;
+                }
+                else if(randomNumber > guessInput || randomNumber < guessInput){
+                    guessInput = await rd.question(`${randomNumber > guessInput ? 'Too row' : 'Too high '}. Guess again: `)
+                    tryingCount++;
+                }
+                else if(guessInput == randomNumber){
+                    console.log(`Success! You got it ${tryingCount} guesses! `)
+                    printSuccess(tryingCount);
+                    break;
                 }
             }
+            if(guessInput != " " && (guessInput == randomNumber)) break;
         }
-        if(tryingcount == 1){
-            console.log(`You're a mind reader!`)
-        } else if(tryingcount >=2 && tryingcount <=4){
-            console.log(`Most impressive.`)
-        } else if(tryingcount >=5 && tryingcount <=6){
-            console.log(`You can do better than that.`)
-        } else {
-            console.log(`Better luck next time.`)
-        }
-    }
 
+        if (!(await askToRetry())) {
+            console.log('good bye.');
+            break;
+        }
+
+    }
 
     rd.close();
 })();
+
+function printSuccess(tryingCount) {
+    if(tryingCount == 1){
+        console.log(`You're a mind reader!`)
+    } else if(tryingCount >=2 && tryingCount <=4){
+        console.log(`Most impressive.`)
+    } else if(tryingCount >=5 && tryingCount <=6){
+        console.log(`You can do better than that.`)
+    } else {
+        console.log(`Better luck next time.`)
+    }
+}
+
+async function askToRetry() {
+    const tryAgain = await rd.question('play again? ');
+    return tryAgain && tryAgain.toLowerCase() === 'y';
+}
